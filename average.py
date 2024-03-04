0d7652452090282e36dc71b32a0077dfce4f7a58
eshasad <159801851+eshasad@users.noreply.github.com>
2024-03-03 19:03:17 -0500

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/easad/Desktop/average....py ================
+How many test scores would you like to average? 3
+Enter test score: 50
+Enter test score: 60
+Enter test score: 70
+The average for the test scores you entered is: 60.0
